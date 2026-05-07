2de746e33b6c3d1bc7abe77d4d2dc94e598d9ded
Александр <achernykh.612@gmail.com>
2026-05-07 23:24:24 +0700

    correct config.py

--- config_data/config.py
+++ config_data/config.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-if not find_dotenv():
-    exit("Переменные окружения не загружены т.к отсутствует файл .env")
-else:
-    load_dotenv()
+dotenv_path = find_dotenv()
+if dotenv_path:
+    load_dotenv(dotenv_path)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN не задан в переменных окружения")
 
 DEFAULT_COMMANDS = [
     ("start", "Запустить бота"),
